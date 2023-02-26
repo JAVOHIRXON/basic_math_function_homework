6bea82173a0b061d04ff24741f773bb3ff2534b0
JAVOHIRXON <kamongaron.j@gmail.com>
2023-02-26 16:24:05 +0500

    19 bo'ldim

--- basic_func19.py
+++ basic_func19.py
@@ -1,4 +1,5 @@
-def main(a, b):
+import math
+def main(a=4, b=11):
     '''find the absolute value of the difference between a and b. Return it.
     
     Args:
@@ -8,4 +9,8 @@ def main(a, b):
     Returns:
         int: the result.
     '''
-    return
+    s = a-b
+    x =abs(s)
+
+    return x
+print(main())
